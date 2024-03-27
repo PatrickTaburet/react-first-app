e56d9c2b0970e033af66ad3c16ca3b0f94102614
Patrick Taburet <taburet.patrick@gmail.com>
2024-03-27 11:15:17 +0100

    Before loading new files

--- src/App.js
+++ src/App.js
@@ -8,12 +8,27 @@ class App extends Component {
     color: "green"
   }
   handleClick = () => {
-    if (this.state.titre == "Mon catalogue de voitures"){
+    if (this.state.titre !== "I want to break FREE"){
       this.setState({titre: "I want to break FREE"})
     } else {
       this.setState({titre:"Mon catalogue de voitures"});
     }
-    
+  }
+
+  changeTitle = (title) => {
+    this.setState({
+      titre : title
+    })
+  }
+  changeViaBind = (param) => {
+    this.setState({
+      titre : param
+    })
+  }
+  changeViaInput = (e) => {
+    this.setState({
+      titre : e.target.value
+    })
   }
   render(){
     return (
@@ -23,6 +38,8 @@ class App extends Component {
           color= {this.state.color}
         />
         <button onClick={this.handleClick}>Change the Title</button>
+        <button onClick={this.changeViaBind.bind(this, "I sing under the rain" )}>Change again</button>
+        <input type='text' onChange={this.changeViaInput} value={this.state.titre}></input>
       </div>
     );
   }
